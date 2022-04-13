77ab17d2453015763697392467925400bd4cc699
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-04-13 20:52:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('𝐀𝐫𝐜𝐭𝐢𝐜 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.\nsibssssssss', {
+say('𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.\nsibssssssss', {
   font: 'chrome',
   align: 'center',
   gradient: ['green', 'green']
