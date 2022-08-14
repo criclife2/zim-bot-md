78a7cc54c643958c23cafb71d234350086319c55
criclife2 <107859390+criclife2@users.noreply.github.com>
2022-08-14 09:52:27 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         .catch(async _ => await instagramdlv2(args[0]))
         .catch(async _ => await instagramdlv3(args[0]))
         .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
+    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `⫺╤╤╧ *𝘔𝘪𝘴𝘴 𝘘𝘦𝘦𝘯 𝘞𝘈 𝘣𝘰𝘵* ╧╤╤⫹\n  ‍   ‍  ‍  ‍ ‍  ‍   ‍  ‍  ‍  \n🔗 *Url:* ${url}`, m)
 }
 handler.help = ['ig'].map(v => v + '')
 handler.tags = ['downloader']
