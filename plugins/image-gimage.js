a2b8ee6e1949a2f803ad4614c696370b6ac7cd64
criclife2 <107859390+criclife2@users.noreply.github.com>
2022-08-23 11:03:15 +0530

    Update image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -3,20 +3,12 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
     const res = await googleImage(text)
     conn.sendFile(m.chat, res.getRandom(), 'gimage.jpg', `
-let buttons = [
-  {buttonId: `gimage ${text}`, buttonText: {displayText: 'Next Image'}, type: 1}
-   ]
-   let buttonMessage = {
-  image: { url: images },
-  caption: `❰ 𝗚𝗢𝗢𝗚𝗟𝗘 𝗜𝗠𝗔𝗚𝗘 ❱
-*𝗤𝗨𝗘𝗥𝗬* : ${text}
-*𝗠𝗘𝗗𝗜𝗔 𝗨𝗥𝗟* : ${images}`,
 *── 「 GOOGLE IMAGE 」 ──*
 
 Result from *${text}*
 `.trim(), m)
 }
-handler.help = ['gimage', 'image']
+handler.help = ['gimage', 'img']
 handler.tags = ['internet', 'tools']
 handler.command = /^(gimage|image)$/i
 
