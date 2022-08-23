6b83455d9aaede5cd510ecde6589938e7ce848f3
criclife2 <107859390+criclife2@users.noreply.github.com>
2022-08-23 10:48:46 +0530

    Update image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,12 +1,23 @@
 import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
-    const res = await googleImage(text)
-    conn.sendFile(m.chat, res.getRandom(), 'gimage.jpg', `
-*── 「 GOOGLE IMAGE 」 ──*
-
-Result from *${text}*
-`.trim(), m)
+if (!text) throw `Example : ${prefix + command} ml nana`
+let gis = require('g-i-s')
+gis(text, async (error, result) => {
+n = result
+images = n[Math.floor(Math.random() * n.length)].url
+let buttons = [
+  {buttonId: `gimage ${text}`, buttonText: {displayText: 'Next Image'}, type: 1}
+   ]
+   let buttonMessage = {
+  image: { url: images },
+  caption: `❰ 𝗚𝗢𝗢𝗚𝗟𝗘 𝗜𝗠𝗔𝗚𝗘 ❱
+*𝗤𝗨𝗘𝗥𝗬* : ${text}
+*𝗠𝗘𝗗𝗜𝗔 𝗨𝗥𝗟* : ${images}`,
+  footer: ZimBotInc.user.name,
+  buttons: buttons,
+  headerType: 4
+   }
+   ZimBotInc.sendMessage(m.chat, buttonMessage, { quoted: m })
+})
 }
 handler.help = ['gimage', 'image']
 handler.tags = ['internet', 'tools']
